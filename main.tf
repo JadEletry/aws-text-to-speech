1822b0d41790ed1ca628df1d7192d45b19caaf65
JadEletry <jadeletry78@gmail.com>
2025-06-17 13:25:21 -0400

    fixing terraform file

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_s3_bucket" "tts_bucket" {
   }
 }
 
-<<<<<<< HEAD
+
 resource "aws_s3_bucket_policy" "tts_bucket_policy" {
   bucket = aws_s3_bucket.tts_bucket.id
 
@@ -44,8 +44,6 @@ resource "aws_s3_bucket_policy" "tts_bucket_policy" {
   })
 }
 
-=======
->>>>>>> e6652f01fbb25e32f0c64759e4233adad403cae6
 resource "aws_iam_role" "lambda_role" {
   name = "lambda-execution-role"
 
@@ -86,7 +84,6 @@ resource "aws_lambda_function" "tts_lambda" {
   handler          = "lambda_function.lambda_handler"
   runtime          = "python3.8"
   source_code_hash = filebase64sha256("lambda_function.zip")
-<<<<<<< HEAD
 
   timeout = 15
 
@@ -96,8 +93,6 @@ resource "aws_lambda_function" "tts_lambda" {
     }
   }
 
-=======
->>>>>>> e6652f01fbb25e32f0c64759e4233adad403cae6
 }
 
 resource "aws_lambda_permission" "allow_invoke" {
@@ -106,12 +101,10 @@ resource "aws_lambda_permission" "allow_invoke" {
   function_name = aws_lambda_function.tts_lambda.function_name
   principal     = "lambda.amazonaws.com"
 }
-<<<<<<< HEAD
 
 output "lambda_function_name" {
   value = aws_lambda_function.tts_lambda.function_name
 }
 
 
-=======
->>>>>>> e6652f01fbb25e32f0c64759e4233adad403cae6
+
